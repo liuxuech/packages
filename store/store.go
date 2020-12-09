99ab61a5c8ea63692760e45265c2754eb4f7a3af
liuxuech <yiwaicanxue@163.com>
2020-12-09 11:50:50 +0800

    update

--- store/store.go
+++ store/store.go
@@ -1,5 +1,7 @@
 package store
 
+import "github.com/liuxuech/packages/store/bolt"
+
 type Store interface {
 	Read(key string, opts ...ReadOption) (*Record, error)
 	Write(r *Record, opts ...WriteOption) error
@@ -11,4 +13,29 @@ type Store interface {
 type Record struct {
 	Key   string `json:"key"`
 	Value []byte `json:"value"`
+}
+
+var defaultStore Store
+
+func Read(key string, opts ...ReadOption) (*Record, error) {
+	return defaultStore.Read(key, opts...)
+}
+
+func Write(r *Record, opts ...WriteOption) error {
+	return defaultStore.Write(r, opts...)
+}
+
+func Delete(key string, opts ...DeleteOption) error {
+	return defaultStore.Delete(key, opts...)
+}
+
+func Close() error {
+	return defaultStore.Close()
+}
+
+func InitStore(dbFile string) (err error) {
+	if defaultStore, err = bolt.NewStore(WithDatabase(dbFile)); err != nil {
+		return err
+	}
+	return nil
 }
