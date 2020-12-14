f183f0c680ee549e869eb0468663242022152a1c
liuxuech <yiwaicanxue@163.com>
2020-12-14 15:18:05 +0800

    update

--- sms/sms.go
+++ sms/sms.go
@@ -6,7 +6,7 @@ type Sms interface {
 
 type MessageOption struct {
 	Phones     string `validate:"required=true"` // 被推送短信的手机号，格式：15612345678,1341234567
-	TemplateID string `validate:"required=true"` // 短信模板码
+	TemplateID string `validate:"required=true"` // 短信模板ID
 
 	Sign          string // 签名
 	TemplateParam string // 模板参数
