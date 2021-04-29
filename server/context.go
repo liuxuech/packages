414ddb0b67c84753a92faea4622efd67224fcce8
liuxuech <yiwaicanxue@163.com>
2021-04-29 15:59:08 +0800

    update

--- server/context.go
+++ server/context.go
@@ -2,14 +2,38 @@ package server
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"sync"
 )
 
-type HandlerFunc func(*Context)
+var pool = sync.Pool{New: func() interface{} {
+	return new(Context)
+}}
 
 type Context struct {
 	*gin.Context
 }
 
-func (ctx *Context) Success(code int, data interface{}) {
-	ctx.JSON(code, data)
+// Success 扩展方法
+func (c *Context) Success(data interface{}) {
+	c.JSON(http.StatusOK, gin.H{"data": data})
+}
+
+func (c *Context) reset() {
+	c.Context = nil
+}
+
+// WrapHandlerFunc 包装函数
+type WrapHandlerFunc func(*Context)
+
+// Wrap 包装
+func Wrap(h WrapHandlerFunc) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		context := pool.Get().(*Context) // 从缓冲池拿Context对象
+		context.Context = c
+		h(context)
+		// 重置context
+		context.reset()
+		pool.Put(context) //  把Context对象放回缓冲池
+	}
 }
